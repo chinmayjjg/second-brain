a229541cabea57916c5ca0f797296d33e2f2e0b9
chinmay pradhan <147341405+chinmayjjg@users.noreply.github.com>
2025-08-13 11:29:19 +0530

    fixed cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*", // or your frontend domain
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 
 // Routes
