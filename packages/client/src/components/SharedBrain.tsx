f237101dd9aa731eeada5a4d7cca1de481779420
chinmay pradhan <147341405+chinmayjjg@users.noreply.github.com>
2025-08-13 15:42:52 +0530

    fixed share option

--- packages/client/src/components/SharedBrain.tsx
+++ packages/client/src/components/SharedBrain.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Brain, LinkIcon, FileText, Video, StickyNote } from 'lucide-react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://second-brain-7mvv.onrender.com/api';
 
 interface SharedBrainData {
   brain: {
